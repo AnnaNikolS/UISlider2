6c19fb4d58942a80985b996d1805156ca1177092
AnnaNikolS <137338922+AnnaNikolS@users.noreply.github.com>
2024-02-21 01:47:30 +0300

    Screen colour transfer to view

--- UISlider2/ViewControllers/MainViewController.swift
+++ UISlider2/ViewControllers/MainViewController.swift
@@ -8,10 +8,25 @@
 import UIKit
 
 final class MainViewController: UIViewController {
-
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
+        view.backgroundColor = .systemOrange
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let settingsVC = segue.destination as? SettingsViewController {
+            settingsVC.backgroundColorView = view.backgroundColor
+        } else { return }
         
     }
 }
 
+protocol SettingsViewControllerDelegate: AnyObject {
+    func setViewColor(_ color: UIColor!)
+}
+
+extension MainViewController: SettingsViewControllerDelegate {
+    func setViewColor(_ color: UIColor!) {
+        view.backgroundColor = color
+    }
+}
--- UISlider2/ViewControllers/SettingsViewController.swift
+++ UISlider2/ViewControllers/SettingsViewController.swift
@@ -9,7 +9,8 @@ import UIKit
 
 final class SettingsViewController: UIViewController {
     
-    @IBOutlet weak var colorView: UIView!
+    //MARK: - IBOutlets
+    @IBOutlet weak var colorSettingsView: UIView!
     
     @IBOutlet weak var redLabel: UILabel!
     @IBOutlet weak var greenLabel: UILabel!
@@ -23,11 +24,52 @@ final class SettingsViewController: UIViewController {
     @IBOutlet weak var greenTextField: UITextField!
     @IBOutlet weak var blueTextField: UITextField!
     
+    var redValue: CGFloat = 0
+    var greenValue: CGFloat = 0
+    var blueValue: CGFloat = 0
     
+    var backgroundColorView: UIColor!
+    var delegate: SettingsViewControllerDelegate!
     
+    //MARK: - View Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        colorSettingsView.layer.cornerRadius = 15
+        
+        redSlider.tintColor = .red
+        greenSlider.tintColor = .green
+        
+        colorSettingsView.backgroundColor = backgroundColorView
      
     }
+    
+    
+    
+    @IBAction func pressedSlider(_ sender: UISlider) {
+        
+        switch sender {
+        case redSlider:
+            redValue = CGFloat(round(redSlider.value  *  100) / 100)
+            redLabel.text = redValue.formatted()
+        case greenSlider:
+            greenValue = CGFloat(round(greenSlider.value * 100) / 100)
+            greenLabel.text = greenValue.formatted()
+        default:
+            blueValue = CGFloat(round(blueSlider.value * 100) / 100)
+            blueLabel.text = blueValue.formatted()
+            
+        }
+        
+            colorSettingsView.backgroundColor = UIColor(
+                cgColor: .init(
+                    red: redValue,
+                    green: greenValue,
+                    blue: blueValue,
+                    alpha: 1)
+            )
+        
+        
+    }
+    
 }
